7d6f1f6b7376eeaacdd144779351e47e43026814
jkuya <kuyajesee@proton.me>
2024-04-10 18:10:50 +0300

    A simple text editor

--- punctuation.go
+++ punctuation.go
@@ -1,19 +1,34 @@
 package goreloaded
 
-import "strings"
+import (
+	"strings"
+)
 
 func Punctuate(str []string) string {
 	punctuation := ""
+
 	for i, v := range str {
 		for _, val := range v {
 			if val == '.' || val == ',' || val == '!' || val == '?' || val == ':' || val == ';' {
 				punctuation += string(val)
-				str[i-1] += punctuation
-				str[i] = strings.Trim(str[i], punctuation)
 			}
-			punctuation = ""
+		}
+		if len(punctuation) == 1 {
+			str[i] = strings.Trim(str[i], punctuation)
+			str[i] += punctuation
+		} else if len(punctuation) > 1 {
+			str[i-1] += punctuation
+			str[i] = strings.Trim(str[i], punctuation)
+		}
+		punctuation = ""
+	}
+	for i, v := range str {
+		if v == "." || v == "," || v == "!" || v == "?" || v == ":" || v == ";" {
+			str[i-1] += v
+			str[i] = ""
 		}
 	}
+
 	finalStr := strings.Join(str, " ")
 	return finalStr
 }
