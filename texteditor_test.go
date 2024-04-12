32f649d41828a70f92b69d6c9c30ec097217ed00
jkuya <kuyajesee@proton.me>
2024-04-12 20:28:35 +0300

    Understanding unit testing

--- texteditor_test.go
+++ texteditor_test.go
@@ -2,16 +2,26 @@ package goreloaded_test
 
 import (
 	"testing"
-
-	"goreloaded"
 )
 
-func TestPunctuate(t *testing.T) {
-	expected := "Punctuation tests are... kinda boring, don't you think!?"
+var testcases = []struct {
+	name     string
+	got      string
+	expected string
+}{
+	{"(hex)", "1E (hex) files were added", "30 files were added"},
+	{"(bin)", "It has been 10 (bin) years", "It has been 2 years"},
+}
 
-	got := goreloaded.TextEditor("Punctuation tests are ... kinda boring ,don't you think ! ?")
+func TestTextEditor(t *testing.T) {
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			expected := tc.expected
+			got := tc.got
 
-	if got != expected {
-		t.Errorf("Try again")
+			if got != expected {
+				t.Errorf("%v not %v", expected, got)
+			}
+		})
 	}
 }
