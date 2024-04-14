6a41042d17369e75bc6973c8ae6c99171b2e88ff
jkuya <kuyajesee@proton.me>
2024-04-14 20:10:25 +0300

    Handling errors and documenting

--- punctuation2.go
+++ punctuation2.go
@@ -2,7 +2,8 @@ package goreloaded
 
 import "strings"
 
-// Punctuate returns a string which is correctly puctuated.
+// Punctuate2 returns a string which is correctly puctuated.
+// This Punctuate2 is run last to ensure correct punctuation ignoring all apostrophe characters
 func Punctuate2(str []string) string {
 	var punctuation string
 	for i, v := range str {
--- separate.go
+++ separate.go
@@ -3,7 +3,8 @@ package goreloaded
 import "regexp"
 
 //Separate is used to search for occurence of the format strings eg (up) and (cap, 2)
-// It then ensures there is a space before and after the format strings 
+// It then ensures there is a space before and after the format strings
+// Separate is necessary because we run punctuation firt meaning a punction might be attached to the format string 
 
 func Separate(str string) string {
 	test1 := regexp.MustCompile(`\(\w+,\s+\d+\)`)
